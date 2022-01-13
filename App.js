4e3a9a0e9639f9f80196308826a5712219170c60
Mike-Csta <92047998+Mike-Csta@users.noreply.github.com>
2022-01-13 22:32:34 +0100

    Boczne karty

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import React from "react";
 import Home from "./Pages/Home/Home";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./Tabs";
+const Tab = createMaterialTopTabNavigator();
+
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
