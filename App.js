162b340404fc5bf50c0ab76615ef1d5f006096e4
IceThief <wiktorwpplka@gmail.com>
2022-01-31 13:53:01 +0100

    qwe

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import Homepage from "./Homepage";
-import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react"
+import { StatusBar } from "expo-status-bar"
+import Homepage from "./Pages/Homepage/Homepage"
+import { View, Text, StyleSheet, Image } from "react-native"
+
 export default function App() {
   return (
     <View>
       <StatusBar style="light" />
       <Homepage />
     </View>
-  );
+  )
 }
---
+++ Pages/Homepage/comp/Custom_Buttons/Custom_Buttons.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { View, Text, StyleSheet, Image } from "react-native"
+
+const Custom_Buttons = () => {
+  return (
+    <View style={style.main}>
+      <Text>Button Modułu</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  main: { width: "100%", flex: 1 },
+})
+
+export default Custom_Buttons
