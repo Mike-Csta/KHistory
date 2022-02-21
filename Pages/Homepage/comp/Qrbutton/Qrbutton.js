5c6bc72056d05cfad331356250a9fede713e29c8
Mike-Csta <92047998+Mike-Csta@users.noreply.github.com>
2022-02-21 22:51:02 +0100

    Zmiana na ludzie i historia i po boku QR

--- Pages/Homepage/comp/Qrbutton/Qrbutton.js
+++ Pages/Homepage/comp/Qrbutton/Qrbutton.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TriangleCorner,
+} from "react-native";
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import {
   responsiveNumber,
@@ -7,24 +14,86 @@ import {
 } from "react-native-responsive-number";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import scan from "../../../../src/scan.png";
+import lih from "../../../../src/LIH.png";
 
 const Qrbutton = (props) => {
   return (
-    <TouchableOpacity onPress={() => props.navigation.navigate("Scan")}>
+    <View>
       <View style={style.centerTop}></View>
       <View style={style.center}>
-        <View style={style.left}>
-          <Image source={scan} style={style.image}></Image>
-        </View>
-        <View style={style.right}>
-          <Text style={style.text}>KOD PRZYSTANKU</Text>
-        </View>
+        <TouchableOpacity
+          style={style.t1}
+          onPress={() => props.navigation.navigate("Scan")}
+        >
+          <View style={style.left}>
+            <Image source={lih} style={style.image}></Image>
+          </View>
+          <View style={style.right}>
+            <Text style={style.text}>LUDZIE I HISTORIA</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={style.t}
+          onPress={() => props.navigation.navigate("Scan")}
+        >
+          <View style={style.right_2}>
+            <View style={style.right_2_top}>
+              <Text style={style.text_2}>KOD</Text>
+            </View>
+            <View style={style.right_2_center}>
+              <Image source={scan} style={style.image_2}></Image>
+            </View>
+            <View style={style.right_2_bottom}>
+              <Text style={style.text_3}>PRZYSTENKU</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
       </View>
-    </TouchableOpacity>
+    </View>
   );
 };
 
 const style = StyleSheet.create({
+  t1: {
+    display: "flex",
+    flexDirection: "row",
+    width: "80.1%",
+  },
+  right_2_top: {
+    height: responsiveNumber(15),
+    width: "100%",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  right_2_center: {
+    height: responsiveNumber(40),
+    width: "100%",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  right_2_bottom: {
+    height: responsiveNumber(15),
+    width: "100%",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  right_2: {
+    width: responsiveNumber(70),
+    flex: 1,
+    borderBottomWidth: 100,
+    borderBottomColor: "#994239",
+    borderLeftWidth: 30,
+    borderLeftColor: "transparent",
+    borderRightWidth: 0,
+    borderStyle: "solid",
+
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+  },
   main: {
     display: "flex",
     flexDirection: "column",
@@ -56,21 +125,21 @@ const style = StyleSheet.create({
     borderRadius: responsiveNumber(18),
     opacity: 1,
     height: responsiveNumber(70),
+    overflow: "hidden",
   },
   text: {
     color: "white",
-    fontSize: RFValue(18),
-    letterSpacing: responsiveLetterSpacing(210, 20),
+    fontSize: RFValue(14),
+    letterSpacing: responsiveLetterSpacing(270, 14),
     opacity: 0.93,
   },
   left: {
     display: "flex",
-    // backgroundColor: "red",
     textAlign: "center",
     justifyContent: "center",
     alignItems: "center",
 
-    flex: 0.27,
+    flex: 0.43,
   },
   right: {
     textAlign: "center",
@@ -78,7 +147,40 @@ const style = StyleSheet.create({
     alignItems: "center",
     flex: 1,
   },
+
+  text_2: {
+    color: "white",
+    fontSize: RFValue(6.5),
+    letterSpacing: responsiveLetterSpacing(90, 6.5),
+    opacity: 0.93,
+    // backgroundColor: "red",
+    marginRight: responsiveNumber(8),
+    marginTop: responsiveNumber(7),
+    width: responsiveNumber(30),
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text_3: {
+    color: "white",
+    fontSize: RFValue(6.5),
+    letterSpacing: responsiveLetterSpacing(90, 6.5),
+    opacity: 0.93,
+    // backgroundColor: "red",
+    marginRight: responsiveNumber(20),
+    marginBottom: responsiveNumber(7),
+    width: responsiveNumber(70),
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   image: { height: responsiveNumber(50), aspectRatio: 1, opacity: 0.68 },
+  image_2: {
+    height: responsiveNumber(27),
+    aspectRatio: 1,
+    opacity: 0.68,
+    marginRight: responsiveNumber(8),
+  },
 });
 
 export default Qrbutton;
