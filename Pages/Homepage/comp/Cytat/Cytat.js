cf03b78410127ba663d9b7f7ac9d69e41cd5155a
Mike-Csta <92047998+Mike-Csta@users.noreply.github.com>
2022-03-21 23:10:27 +0100

    dziala

--- Pages/Homepage/comp/Cytat/Cytat.js
+++ Pages/Homepage/comp/Cytat/Cytat.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { View, Text, StyleSheet, Image, Animated } from 'react-native'
-import { CytatApi } from './api'
-import { Dimensions } from 'react-native'
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, StyleSheet, Image, Animated } from "react-native";
+import { CytatApi } from "./api";
+import { Dimensions } from "react-native";
 import {
   responsiveNumber,
   responsiveLetterSpacing,
-} from 'react-native-responsive-number'
+} from "react-native-responsive-number";
 const Cytat = (props) => {
-  let cytatNr = Math.floor(Math.random() * 5 + 1)
+  let cytatNr = Math.floor(Math.random() * 5 + 1);
 
   return (
     <View style={style.main}>
@@ -27,24 +27,24 @@ const Cytat = (props) => {
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const style1 = StyleSheet.create({
   main: {
-    width: '100%',
+    width: "100%",
     flex: 1,
     //  backgroundColor: "gray"
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
     width: responsiveNumber(290),
   },
   cytat: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     lineHeight: responsiveNumber(23),
     fontSize: responsiveNumber(15),
     letterSpacing: responsiveLetterSpacing(80, 15),
@@ -52,39 +52,39 @@ const style1 = StyleSheet.create({
     // backgroundColor: "red",
   },
   line: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     borderWidth: 1,
-    borderColor: '#454040',
+    borderColor: "#454040",
     height: responsiveNumber(1),
     marginTop: responsiveNumber(22),
     marginBottom: responsiveNumber(17),
     opacity: 0.5,
   },
   autor: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     lineHeight: responsiveNumber(20),
     fontSize: responsiveNumber(13),
     letterSpacing: responsiveLetterSpacing(80, 13),
   },
-})
+});
 
 const style2 = StyleSheet.create({
   main: {
-    width: '100%',
+    width: "100%",
     flex: 1,
     //  backgroundColor: "gray"
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
     width: responsiveNumber(290),
   },
   cytat: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     lineHeight: responsiveNumber(20),
     fontSize: responsiveNumber(10),
     letterSpacing: responsiveLetterSpacing(80, 15),
@@ -92,32 +92,32 @@ const style2 = StyleSheet.create({
     // backgroundColor: "red",
   },
   line: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     borderWidth: 1,
-    borderColor: '#454040',
+    borderColor: "#454040",
     height: responsiveNumber(1),
     marginTop: responsiveNumber(10),
     marginBottom: responsiveNumber(8),
     opacity: 0.5,
   },
   autor: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     lineHeight: responsiveNumber(20),
     fontSize: responsiveNumber(9),
     letterSpacing: responsiveLetterSpacing(80, 13),
   },
-})
-let style = style1
-const windowWidth = Dimensions.get('window').width
-const windowHeight = Dimensions.get('window').height
+});
+let style = style1;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 // console.log(windowWidth, windowHeight)
 
 if (windowHeight / windowWidth > 1.8) {
-  style = style1
+  style = style1;
 } else {
-  style = style2
+  style = style2;
 }
 
-export default Cytat
+export default Cytat;
