5984684aee3bcc99a42acfa4753484af0fcaca41
IceThief <wiktorwpplka@gmail.com>
2022-03-22 18:32:49 +0100

    .

--- App.js
+++ App.js
@@ -1,33 +1,33 @@
-import { useFonts } from "expo-font";
-import React, { useEffect } from "react";
-import Scan from "./Pages/Scan/Scan";
-import PeopleAndHis from "./Pages/PeopleAndHis/PeopleAndHis";
-import Homepage from "./Pages/Homepage/Homepage";
-import { View, Text, StyleSheet, Image } from "react-native";
-import AppLoading from "expo-app-loading";
-import Web from "./Pages/Homepage/comp/Custom_Buttons/Web";
-import Web2 from "./Pages/Homepage/comp/Custom_Buttons/Web2";
-import Custom_Buttons from "./Pages/Homepage/comp/Custom_Buttons/Custom_Buttons";
+import { useFonts } from 'expo-font'
+import React, { useEffect } from 'react'
+import Scan from './Pages/Scan/Scan'
+import PeopleAndHis from './Pages/PeopleAndHis/PeopleAndHis'
+import Homepage from './Pages/Homepage/Homepage'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import AppLoading from 'expo-app-loading'
+import Web from './Pages/Homepage/comp/Custom_Buttons/Web'
+import Web2 from './Pages/Homepage/comp/Custom_Buttons/Web2'
+import Custom_Buttons from './Pages/Homepage/comp/Custom_Buttons/Custom_Buttons'
 import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
-} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    BalooBhaijaan2: require("./src/BalooBhaijaan2.ttf"),
-  });
+    BalooBhaijaan2: require('./src/BalooBhaijaan2.ttf'),
+  })
 
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return <AppLoading />
   }
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#222" }}>
+    <View style={{ flex: 1, backgroundColor: '#222' }}>
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Home"
@@ -44,5 +44,6 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     </View>
-  );
+  )
 }
+// fjdlsjlk
